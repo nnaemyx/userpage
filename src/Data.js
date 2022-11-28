ced4c0b3879eda7ca9a26f16c9072773d168b9bf
tezon <tezonteam@gmail.com>
2022-11-28 00:39:52 -0800

    first commit

--- src/Data.js
+++ src/Data.js
@@ -1,5 +1,7 @@
 import Profilepic from "./images/Profile Pic Display (1).svg";
 import Editicon from "./images/arrow-down-to-line.svg";
+import Adsimage from "./images/pic2 (1).png";
+
 
 export const profile = [
   {
@@ -26,5 +28,32 @@ export const files = [
     name: "Resume/CV",
     docs: "Google Docs",
     icon: <Editicon />,
+  },
+];
+
+export const people = [
+  {
+    image: <Adsimage />,
+    name: "James Tom",
+    paragraph: "Software Developer",
+    follow: "follow",
+  },
+  {
+    image: <Adsimage />,
+    name: "James Tom",
+    paragraph: "Software Developer",
+    follow: "follow",
+  },
+  {
+    image: <Adsimage />,
+    name: "James Tom",
+    paragraph: "Software Developer",
+    follow: "follow",
+  },
+  {
+    image: <Adsimage />,
+    name: "James Tom",
+    paragraph: "Software Developer",
+    follow: "follow",
   },
 ];
--- src/components/Adssection/Ads.jsx
+++ src/components/Adssection/Ads.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import './Ads.css'
+import { people } from '../../Data'
+
 
 const Ads = () => {
   return (
-    <div>Ads</div>
+    <div className='ads_section'>
+      <div className='ads'>
+        <h1>ADS</h1>
+      </div>
+
+      <div className='ads_people'>
+        <h1>People also Viewed</h1>
+          {people.map((items, index) => {
+          return (
+            <div className="ads_body">
+              <div>
+                <img src={items.image.type} alt="" />
+              </div>
+
+              <div className='ads_text'>
+                <div>
+                  <h1 className="ads_name">{items.name}</h1>
+                  <p className=" ads_paragraph">{items.paragraph}</p>
+                  <button>{items.follow}</button>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+
+    </div>
   )
 }
 
